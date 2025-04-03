e9653189bd072cb52f2dc91ea48bb51f9a0ea176
Pedro Barreto <pbdeoliveira@gmail.com>
2025-04-03 10:45:55 -0300

    feat: new user types

--- backend/src/types/User.ts
+++ backend/src/types/User.ts
@@ -3,9 +3,14 @@ export type OwnerUser = {
   name: string;
   email: string;
   password: string;
+  cnpj: string;
+  brand: string;
+  approved_payment_methods: ('pix' | 'dinheiro' | 'credit_card')[];
 };
 
+
 export type CustomerUser = {
+  name: string;
   role: 'customer';
   phone: string;
 };
